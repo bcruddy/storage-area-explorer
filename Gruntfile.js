262a1c94ae66102e995cd3efe27ea2e9e6d29621
brian ruddy <briancruddy@gmail.com>
2017-09-20 08:21:37 -0400

    include grunt-cli locally, add nvmrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,5 @@ module.exports = function (grunt) {
     // Default task(s).
     grunt.registerTask('default', ['clean', 'karma', 'zip']);
 
-    grunt.registerTask("ci", ['clean', 'bower','karma', 'zip'])
-
+    grunt.registerTask('ci', ['clean', 'bower', 'karma', 'zip']);
 };
